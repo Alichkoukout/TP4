278879199f3ae0fce7c4195db0b5a1a0c476d64d
Alichkoukout <117859543+Alichkoukout@users.noreply.github.com>
2025-10-16 11:32:00 +0100

    Add files via upload

---
+++ TP4/src/main/java/entities/Machine.java
@@ -0,0 +1,70 @@
+package entities;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@NamedNativeQuery(name = "findBetweenDateNative", query = "select * from machine where dateAchat between :d1 and :d2", resultClass = Machine.class)
+@NamedQuery(name = "findBetweenDate", query = "from Machine where dateAchat between :d1 and :d2")
+public class Machine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public Machine(String m123, Date dateAchat, Salle byId) {
+    }
+
+    public Machine() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public Date getDateAchat() {
+        return dateAchat;
+    }
+
+    public void setDateAchat(Date dateAchat) {
+        this.dateAchat = dateAchat;
+    }
+
+    public Salle getSalle() {
+        return salle;
+    }
+
+    public void setSalle(Salle salle) {
+        this.salle = salle;
+    }
+
+    public Machine(int id, Date dateAchat, String ref, Salle salle) {
+        this.id = id;
+        this.dateAchat = dateAchat;
+        this.ref = ref;
+        this.salle = salle;
+    }
+
+    private String ref;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateAchat;
+
+    @ManyToOne
+    private Salle salle;
+
+    // Constructeurs, getters et setters
+}
